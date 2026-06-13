f144496e5b47be50be6ff565d5934fda43fcb406
TEJAS M <tejasmanjulakg@gmail.com>
2026-06-13 20:19:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed=new Typed(".text",{
-    strings:["Frontend Developer"],
+    strings:["Python Full Stack Developer"],
         typeSpeed:100,
         backSpeed:100,
         backDelay:1000,
